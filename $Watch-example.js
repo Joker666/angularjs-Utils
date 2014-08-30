77090b5bcd6b78df3928300451f9437e865489b3
Hasan Rafi <mr.k779@outlook.com>
2014-08-31 03:48:35 +0600

    SublimeGit not good

--- $Watch-example.js
+++ $Watch-example.js
@@ -13,22 +13,21 @@ App.service('Tree', function(){
 
 App.directive('treeView', function(Tree){
   return {
-  
+
     restrict: 'A',
-    
+
     link: function(scope, elem, attrs){
       scope.data = [];
 
       scope.$watch(function(){
-      
+
         return Tree.rebuild;
-        
-      }, function(newVal, oldVal){
-      
-        if(newVal) {        
+
+      },function(newVal, oldVal){
+        if(newVal) {
             Tree.Build(function(response){
-         		scope.data = response;
-         		Tree.rebuild = false;
+           		scope.data = response;
+           		Tree.rebuild = false;
             });
         };
       });
