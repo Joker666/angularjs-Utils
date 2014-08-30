35497a019fbe03505b75316c10c9014d33fdc5ad
Hasan Rafi <mr.k779@outlook.com>
2014-08-31 02:53:09 +0600

    =first commit

---
+++ $Watch-example.js
@@ -0,0 +1,37 @@
+var App = app.module('ExampleApp', []);
+
+App.service('Tree', function(){
+  this.rebuild = false;
+
+  this.Build = function(cb){
+    cb(response);
+  };
+
+  return this;
+});
+
+
+App.directive('treeView', function(Tree){
+  return {
+  
+    restrict: 'A',
+    
+    link: function(scope, elem, attrs){
+      scope.data = [];
+
+      scope.$watch(function(){
+      
+        return Tree.rebuild;
+        
+      }, function(newVal, oldVal){
+      
+        if(newVal) {        
+            Tree.Build(function(response){
+         		scope.data = response;
+         		Tree.rebuild = false;
+            });
+        };
+      });
+    }
+  }
+});
